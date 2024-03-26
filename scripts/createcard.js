68124b5bd6b8bb0ae19f0f66053d0fb3b7c91351
Jackson Lofquist <lofquijs@dukes.jmu.edu>
2024-03-26 15:19:01 -0400

    part 1 complete

--- scripts/createcard.js
+++ scripts/createcard.js
@@ -11,3 +11,26 @@ document.getElementById("preview").addEventListener("click", function() {
     document.querySelector(".message-text").textContent = message;
     document.querySelector(".from-text").textContent = from;
   });
+
+  document.getElementById("save").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var cards = localStorage.getItem("cards");
+    if (cards) {
+      cards = JSON.parse(cards); 
+    } else {
+      cards = []; 
+    }
+
+    var newCard = {
+      to: document.getElementById("to").value,
+      from: document.getElementById("from").value,
+      title: document.getElementById("title").value,
+      subtitle: document.getElementById("subtitle").value,
+      message: document.getElementById("message").value
+    };
+
+    cards.push(newCard);
+
+    localStorage.setItem("cards", JSON.stringify(cards));
+  });
